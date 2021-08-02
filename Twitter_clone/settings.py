03ac6e2dc1e8b31e376d76108566abf48d1c9a60
akhad abdullaev <ahadjon.abdullaev1997@gmail.com>
2021-08-02 12:35:12 -0700

    my commit

--- Twitter_clone/settings.py
+++ Twitter_clone/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'l0pd1&-#q=o%o79ian74e=h-#75ma5xwyv%l&!0uhk(eonuxbz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-twitterwebsite.herokuapp.com']
 
 # Application definition
 
